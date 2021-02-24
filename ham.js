78a3159d84edc02cb7b0c8af9b94e3816c29464e
insert github real name heredianaandreaa <insert github email here>
2021-02-24 10:23:23 +0300

    stll need to fix B's JS but everything else works

---
+++ ham.js
@@ -0,0 +1,44 @@
+var navbar = document.querySelector(".navbar")
+var ham = document.querySelector(".ham")
+
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+}
+
+ham.addEventListener("click", toggleHamburger)
+
+var menuLinks = document.querySelectorAll(".menuLink")
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
---
+++ toggle.js
@@ -0,0 +1,194 @@
+window.addEventListener("load", loadScreen);
+
+function loadScreen() {
+    console.log("loadScreen");
+    document.querySelector(".happy").addEventListener("click", openHappy);
+    document.querySelector(".sad").addEventListener("click", openSad);
+    document.querySelector(".angry").addEventListener("click", openAngry);
+}
+
+//open Happy
+
+function openHappy() {
+    console.log("openHappy");
+    document.querySelector(".content").classList.add("hidden");
+
+    document.querySelector(".happyscreen").classList.remove("hidden");
+    document.querySelector(".goback").addEventListener("click", goBack);
+    document.querySelector(".yes").addEventListener("click", openChoice);
+}
+
+function goBack() {
+    console.log("goBack");
+    document.querySelector(".happyscreen").classList.add("hidden");
+    document.querySelector(".content").classList.remove("hidden");
+}
+
+function openChoice() {
+    console.log("openChoice");
+    document.querySelector(".happyscreen").classList.add("hidden");
+    document.querySelector(".happychoice").classList.remove("hidden");
+    document.querySelector(".goback2").addEventListener("click", goBack2);
+    
+    document.querySelector(".game").addEventListener("click", openGame);
+    document.querySelector(".comic").addEventListener("click", openComic);
+    document.querySelector(".read").addEventListener("click", openRead);
+}
+function goBack2() {
+    console.log("goBack2");
+    document.querySelector(".happychoice").classList.add("hidden");
+    document.querySelector(".content").classList.remove("hidden");
+}
+
+function openGame() {
+    console.log("openGame");
+    document.querySelector(".happychoice").classList.add("hidden");
+    document.querySelector(".happygame").classList.remove("hidden");
+    document.querySelector(".goback3").addEventListener("click", goBack3);
+    
+    document.querySelector(".game1").addEventListener("click", loadTic);
+    document.querySelector(".game3").addEventListener("click", loadFlower);
+    
+}
+function goBack3() {
+    console.log("goBack");
+    document.querySelector(".happygame").classList.add("hidden");
+    document.querySelector(".happychoice").classList.remove("hidden");
+}
+function loadTic() {
+    document.querySelector(".happygame").classList.add("hidden");
+    document.querySelector(".happytic").classList.remove("hidden");
+    
+    }
+
+function loadFlower() {
+    console.log("loadFlower");
+    document.querySelector(".happygame").classList.add("hidden");
+    document.querySelector(".happyflower").classList.remove("hidden");
+
+}
+
+//open Sad
+
+function openSad() {
+    console.log("openSad");
+    document.querySelector(".content").classList.add("hidden");
+
+    document.querySelector(".sadscreen").classList.remove("hidden");
+    document.querySelector(".gobacksad").addEventListener("click", goBacksad);
+    document.querySelector(".yessad").addEventListener("click", openChoicesad);
+}
+
+function goBacksad() {
+    console.log("goBacksad");
+    document.querySelector(".sadscreen").classList.add("hidden");
+    document.querySelector(".content").classList.remove("hidden");
+}
+
+function openChoicesad() {
+    console.log("openChoice");
+    document.querySelector(".sadscreen").classList.add("hidden");
+    document.querySelector(".sadchoice").classList.remove("hidden");
+    document.querySelector(".gobacksad2").addEventListener("click", goBacksad2);
+    
+    document.querySelector(".gamesad").addEventListener("click", openGamesad);
+    document.querySelector(".comicsad").addEventListener("click", openComicsad);
+    document.querySelector(".readsad").addEventListener("click", openReadsad);
+}
+function goBacksad2() {
+    console.log("goBacksad2");
+    document.querySelector(".sadchoice").classList.add("hidden");
+    document.querySelector(".content").classList.remove("hidden");
+}
+
+function openGamesad() {
+    console.log("openGamesad");
+    document.querySelector(".sadchoice").classList.add("hidden");
+    document.querySelector(".sadgame").classList.remove("hidden");
+    document.querySelector(".gobacksad3").addEventListener("click", goBacksad3);
+    
+    document.querySelector(".game1sad").addEventListener("click", loadTicsad);
+    document.querySelector(".game3sad").addEventListener("click", loadFlowersad);
+    
+}
+
+function goBacksad3() {
+    console.log("goBacksad3");
+    document.querySelector(".sadgame").classList.add("hidden");
+    document.querySelector(".sadchoice").classList.remove("hidden");
+}
+
+function loadTicsad() {
+    document.querySelector(".sadgame").classList.add("hidden");
+    document.querySelector(".sadtic").classList.remove("hidden");
+
+}
+
+function loadFlowersad() {
+    document.querySelector(".sadgame").classList.add("hidden");
+    document.querySelector(".sadflower").classList.remove("hidden");
+
+}
+
+
+//open Angry
+
+function openAngry() {
+    console.log("openangry");
+    document.querySelector(".content").classList.add("hidden");
+
+    document.querySelector(".angryscreen").classList.remove("hidden");
+    document.querySelector(".gobackangry").addEventListener("click", goBackangry);
+    document.querySelector(".yesangry").addEventListener("click", openChoiceangry);
+}
+
+function goBackangry() {
+    console.log("goBackangry");
+    document.querySelector(".angryscreen").classList.add("hidden");
+    document.querySelector(".content").classList.remove("hidden");
+}
+
+function openChoiceangry() {
+    console.log("openChoiceangry");
+    document.querySelector(".angryscreen").classList.add("hidden");
+    document.querySelector(".angrychoice").classList.remove("hidden");
+    document.querySelector(".gobackangry2").addEventListener("click", goBackangry2);
+
+    document.querySelector(".gameangry").addEventListener("click", openGameangry);
+    document.querySelector(".comicangry").addEventListener("click", openComicangry);
+    document.querySelector(".readangry").addEventListener("click", openReadangry);
+}    
+
+function goBackangry2() {
+console.log("goBackangry2");
+document.querySelector(".angrychoice").classList.add("hidden");
+document.querySelector(".content").classList.remove("hidden");
+}
+
+function openGameangry() {
+    console.log("openGameangry");
+    document.querySelector(".angryscreen").classList.add("hidden");
+    document.querySelector(".angrygame").classList.remove("hidden");
+    document.querySelector(".gobackangry3").addEventListener("click", goBackangry3);
+
+    document.querySelector(".game1angry").addEventListener("click", loadTicangry);
+    document.querySelector(".game3angry").addEventListener("click", loadFlowerangry);
+    
+}    
+
+function goBackangry3() {
+console.log("goBackangry3");
+document.querySelector(".angrygame").classList.add("hidden");
+document.querySelector(".angrychoice").classList.remove("hidden");
+}
+
+function loadTicangry() {
+    document.querySelector(".angrygame").classList.add("hidden");
+    document.querySelector(".angrytic").classList.remove("hidden");
+    
+    }
+function loadFlowerangry() {
+    document.querySelector(".angrygame").classList.add("hidden");
+    document.querySelector(".angryflower").classList.remove("hidden");
+
+}
